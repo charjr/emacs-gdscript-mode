1f77b0de524909f7e5e1c7d5c40b0f70f412e287
John Charman <102669158+charjr@users.noreply.github.com>
2026-05-23 10:35:33 +0100

    Add additional export keywords (#204)

--- gdscript-keywords.el
+++ gdscript-keywords.el
@@ -30,7 +30,15 @@
 
 (defconst gdscript-keywords
   '("and" "as" "assert" "break" "breakpoint" "case" "class" "class_name"
-    "const" "continue" "do" "elif" "else" "enum" "export" "extends" "false" "for" "func" "if" "in" "is"
+    "const" "continue" "do" "elif" "else" "enum"
+    "export" "export_category" "export_color_no_alpha" "export_custom"
+    "export_dir" "export_enum" "export_exp_easing" "export_file"
+    "export_flags"
+    "export_flags_2d_navigation" "export_flags_2d_physics" "export_flags_2d_render"
+    "export_flags_3d_navigation" "export_flags_3d_physics" "export_flags_3d_render"
+    "export_global_dir" "export_global_file" "export_group" "export_multiline"
+    "export_node_path" "export_range" "export_storage" "export_subgroup" "export_tool_button"
+    "extends" "false" "for" "func" "if" "in" "is"
     "master" "match" "not" "onready" "or" "pass" "preload" "puppet" "remote" "remotesync" "return" "self" "setget" "signal"
     "slave" "static" "switch" "sync" "tool" "true" "var" "while" "yield"))
 
